aabffe485952076f7d5904542a8df5a418b62695
Anthony Attard <anthony@anthonyattard.com>
2017-10-31 17:40:25 -0700

    Modified dummy data

--- lotsofmenus.py
+++ lotsofmenus.py
@@ -20,7 +20,7 @@ session = DBSession()
 
 
 # Create dummy user
-User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
+User1 = User(name="Robo Barista", email="test@anthonyattard.com",
              picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
 session.add(User1)
 session.commit()
